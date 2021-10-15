580882bb9ba3b5c27c93f402a4ef2a3a862fe5ce
Jyoti Anil Pawar <91241516+Jyoti4972@users.noreply.github.com>
2021-10-15 20:40:08 +0530

    Add files via upload

---
+++ mSource.cpp
@@ -0,0 +1,225 @@
+#include "iostream"
+using namespace std;
+#include <stdio.h>
+#include<conio.h>
+//int main()
+//{
+//    int i;
+//    int x[10];
+//    cout <<" enter u name";
+//    //cout << "enter ypur no";
+//    for (i = 0; i<9; i++)
+//    {
+//        //cin>> x[i];
+//    }
+//   // cout << "enter ypur no";
+//    //for (i = 0; i < 9; i++)
+//    {
+//       // cout<< x[i]<<endl;
+//    }
+//    int n = 0;
+//    cout << "enter value";
+//    cin >> n;
+//    int* p = new int(n);
+//    cout << "enter ypur no";
+//    for (i = 0; i < n; i++)
+//    {
+//        cin >> x[i];
+//    }
+//    cout << "enter ypur no";
+//    for (i = 0; i < n; i++)
+//    {
+//        cout << x[i]<<endl;
+//    }
+//}
+//int main()
+//{
+//    int i,g = 0;
+//    int* p = new int(10);
+//    cout << "enter element one by one"<<endl;
+//    for (i = 0; i <=10; i++)
+//    {
+//        cin >> g;
+//    }
+//    cout << "my element are  one by one";
+//    for (i = 0; i < g; i++)
+//    {
+//        cout <<i<<endl;
+//    }
+//        return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//printf("\n 1.multiplication");
+//		printf("\n 2.odd even no");
+//		printf("\n 3.printing n number");
+//		printf("\n 4. exit");
+//		printf("\nenter your choice\n");
+
+
+//		scanf_s("\n%d", &choice);
+//
+//
+//
+//
+//		switch (choice)
+//		{
+//		case 1:
+//			printf("enter two number");
+//			scanf_s("%d %d", &a, &b);
+//			c = a * b;
+//			printf("your multiplication is %d", c);
+//			break;
+//		case 2:
+//			printf("enter the number");
+//			scanf_s("%d", &a);
+//			if ((a % 2) == 0)
+//				printf("even no");
+//			else
+//				printf("odd no");
+//			break;
+//		case 3:
+//			printf("enter the number");
+//			scanf_s("%d", &a);
+//			for (b = 1; b <= a; b++)
+//				printf(" %d ", b);
+//			break;
+//		case 4:
+//
+//
+//
+//		default:
+//			printf("wrong choice");
+//
+//		}
+//	}
+//}
+
+
+
+//////////////////////////////////program for atm machine/////////////////////
+//class demo
+//{	 
+//public:
+//	
+//
+//	void fun()
+//		
+//		  
+//	
+//};
+
+/* int main()
+{
+int o, i,  l;
+int k = 1234;
+short int n = 0;
+
+	//int k;
+	//demo o;
+	//o.fun();
+while (1)
+{
+	cout << "\nwelcome cocmos bank" << endl;
+	cout << "insert your atm card & enter 1" << endl;
+	cin >> o;
+	if (o == 1)
+	{
+		cout << "hello onkar vijay bhagawat" << endl;
+		cout << "enter your four digit pin:=";
+		cin >> n;
+
+
+		if ((n == k) && (k == n))
+
+		{
+			//cin >> n;
+			cout << "the pin is:" << n << endl;
+		}
+		else
+		{
+			cout << "password not match plase enter correct paswword";
+			return (1);
+		}
+	}
+		else
+	{
+		cout << "plase insert card propary or enter 1";
+		return 1;
+
+	}
+	
+
+
+	cout << "sellect following opation";
+	cout << "\n(1)Balance inquriry";
+	cout << "\n(2)cash whidraw";
+	cout << "\n(3)chang pin";
+	cout << "\n(4)coustmer care";
+	cout << "\n(5)Exit" << endl;
+	cin >> k;
+
+	switch (k)
+
+	{
+
+	case 1:
+		cout << "\n(1)Balance inquriry" << endl;
+		cout << "your balnce is:12,500";
+		break;
+	case 2:
+		cout << "\n(2)cash whidraw" << endl;
+		cout << "enter cash amount:" << "RS"s;
+		cin >> o;
+		cout << "your ammount is :RS" << o << endl;
+		break;
+	case 3:
+		cout << "\n(3)chang pin" << endl;
+		cout << "enter old pin" << endl;
+		cin >> l;
+		cout << n << l;
+
+		if ((n == l) && (l == n))
+		{
+
+			cout << "enter your new pin:";
+			cin >> i;
+			cout << "your new pin is:" << i;
+
+		}
+		else
+		{
+			cout << "wrong pin please enter correct pin";
+
+		}
+		break;
+	case 4:
+		cout << "(4)coustmer care" << endl;
+		cout << "coustmer care number is 7769087347";
+		break;
+	case 5:
+		return 0;
+
+	default:
+		cout << "wrong option selected";
+		break;
+
+	}
+}
+}
+
+
+
+ */
+
+
